594f5a5f1690ca5461d03c7cd866089e5377aed5
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2025-09-16 17:43:34 +0200

    update

--- install/packaging/lazyvim.sh
+++ install/packaging/lazyvim.sh
@@ -4,5 +4,4 @@ if [[ ! -d "$HOME/.config/nvim" ]]; then
   git clone https://github.com/LazyVim/starter ~/.config/nvim
   cp -R ~/.local/share/omarchy/config/nvim/* ~/.config/nvim/
   rm -rf ~/.config/nvim/.git
-  echo "vim.opt.relativenumber = false" >>~/.config/nvim/lua/config/options.lua
 fi

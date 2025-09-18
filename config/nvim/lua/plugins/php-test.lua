06315a7a5c5a1d2781a4d0ea6ba5a5b9b1466755
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2025-09-18 11:14:29 +0200

    fix nvim

--- config/nvim/lua/plugins/php-test.lua
+++ config/nvim/lua/plugins/php-test.lua
@@ -1,38 +1,38 @@
 return {
-	{
-		"nvim-neotest/neotest",
-		lazy = false,
-		dependencies = {
-			"V13Axel/neotest-pest",
-			"olimorris/neotest-phpunit",
-		},
-		opts = function(_, opts)
-			opts.adapters = opts.adapters or {}
+  {
+    "nvim-neotest/neotest",
+    lazy = false,
+    dependencies = {
+      "V13Axel/neotest-pest",
+      "olimorris/neotest-phpunit",
+    },
+    opts = function(_, opts)
+      opts.adapters = opts.adapters or {}
 
-			table.insert(
-				opts.adapters,
-				require("neotest-pest")({
-					sail_enabled = function()
-						return false
-					end,
-					pest_cmd = function()
-						return "./vendor/bin/pest"
-					end,
-				})
-			)
+      table.insert(
+        opts.adapters,
+        require("neotest-pest")({
+          sail_enabled = function()
+            return false
+          end,
+          pest_cmd = function()
+            return "./vendor/bin/pest"
+          end,
+        })
+      )
 
-			table.insert(
-				opts.adapters,
-				require("neotest-phpunit")({
-					root_ignore_files = { "tests/Pest.php" },
-					phpunit_cmd = function()
-						return "./vendor/bin/phpunit"
-					end,
-					env = {
-						XDEBUG_MODE = "coverage",
-					},
-				})
-			)
-		end,
-	},
+      table.insert(
+        opts.adapters,
+        require("neotest-phpunit")({
+          root_ignore_files = { "tests/Pest.php" },
+          phpunit_cmd = function()
+            return "./vendor/bin/phpunit"
+          end,
+          env = {
+            XDEBUG_MODE = "coverage",
+          },
+        })
+      )
+    end,
+  },
 }
---
+++ migrations/1758142076.sh
@@ -0,0 +1,2 @@
+cp -r ~/.local/share/omarchy/themes/rose-pine-dark ~/.config/omarchy/themes
+cp -r ~/.local/share/omarchy/themes/one-dark-pro ~/.config/omarchy/themes

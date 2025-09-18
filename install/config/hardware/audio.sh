705d5f7152ec0169e914d8e7cbe7c479bd126d15
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2025-09-18 17:59:33 +0200

    fix

--- install/config/hardware/audio.sh
+++ install/config/hardware/audio.sh
@@ -1,2 +1,2 @@
-sudo pacman -Syu pipewire pipewire-alsa pipewire-pulse wireplumber
+sudo pacman -Syu pipewire pipewire-alsa pipewire-pulse wireplumber --noconfirm
 systemctl --user enable --now pipewire pipewire-pulse wireplumber
